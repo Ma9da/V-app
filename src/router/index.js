aafbccf40d2881b5d2eaff476388167289812326
Abdelrahman Mahmoud <abdelrahman.mahmoudd2@gmail.com>
2022-10-26 08:55:24 +0200

    structure files

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import { defineAsyncComponent } from "vue";
+import Home from ""
+
+// Async component
+const lazyRoute = (path) => import(path);
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-
-
-  ]
-})
+    {
+      name: "Home",
+      path: "/Home",
+      component: lazyRoute("@/views/HomeView"),
+    },
+  ],
+});
 
 export default router
